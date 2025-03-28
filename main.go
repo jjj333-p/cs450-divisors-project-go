01cda729ff0a2e1fbec9729ca3365357161fd497
jjj333_p <jjj333.p.1325@gmail.com>
2025-03-27 18:53:27 -1000

    misc debugging and additions

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"bufio"
-	"math"
+	"fmt"
 	"os"
 	"strconv"
 	"strings"
@@ -11,10 +11,16 @@ import (
 )
 
 func prime(n int) bool {
-	k := int(math.Sqrt(float64(n)))
-
-	for iR := range k - 2 {
-		i := iR + 2
+	if n < 2 {
+		return false
+	}
+	if n == 2 {
+		return true
+	}
+	if n%2 == 0 {
+		return false
+	}
+	for i := 3; i*i <= n; i += 2 {
 		if n%i == 0 {
 			return false
 		}
@@ -54,11 +60,16 @@ func main() {
 		k, _ := strconv.Atoi(kStr)
 
 		exp := make([]atomic.Int32, 432)
+		for i := range exp {
+			exp[i].Store(0) // Explicitly initialize to 0
+		}
 
 		//for any iterations we want to run concurrently
 		factorizationWait := new(sync.WaitGroup)
 		//make exp the prime factorization of n!/k!
+		factorizationWait.Add(n - 1)
 		for i := 2; i <= n; i++ {
+			fmt.Println("i", i)
 
 			//n!/(k!(n-k)!)
 			var runs int32 = 1
@@ -68,19 +79,23 @@ func main() {
 
 			if isPrimeArr[i] {
 				exp[i].Add(runs)
+				fmt.Println("exp", exp[i].Load())
+				factorizationWait.Done()
 			} else { //need to do the prime factorization of i if it's not prime
 				//factorize in a coroutine
-				factorizationWait.Add(1)
+
 				go func(n int) {
 					defer factorizationWait.Done()
 
-					//n!/(k!(n-k)!)
+					//first run is n!/k!
+					//second run is n!/(k!(n-k)!)
 					for range runs {
 						ncurr := n
 						h := 2
 						for ncurr != 1 {
 							if isPrimeArr[h] && ncurr%h == 0 {
 								exp[h].Add(-1)
+								fmt.Println("exp", exp[i].Load())
 								ncurr /= h
 							} else {
 								h++
@@ -94,6 +109,11 @@ func main() {
 		//wait for all processing to finish
 		factorizationWait.Wait()
 
+		var res int32 = 1
+		for i := range 432 {
+			res *= exp[i].Load()
+		}
+		fmt.Println(res)
 	}
 
 }
